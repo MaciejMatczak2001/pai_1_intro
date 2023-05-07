8041fbaaa98f5afc4aaa65da144a55c17ee96045
MaciejMatczak2001 <maciej.matczak2001@gmail.com>
2023-05-07 12:02:52 +0200

    Added checking http response

--- app.py
+++ app.py
@@ -1,10 +1,17 @@
+#!/usr/bin/python3
+import urllib
+
 import requests
 
 url = "https://mdn.github.io/learning-area/javascript/oojs/json/superheroes.json"
 
-r = requests.get(url)
-response = r.json()
+
+def checkHTTP(url_link):
+    r = requests.get(url_link)
+    if 400 <= r.status_code < 600:
+        raise Exception("error")
+    return r.json()
 
 
 if __name__ == '__main__':
-    print(response)
+    print(checkHTTP(url))
