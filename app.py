bb03cc3911a300c5dc58e847e8bafc4e9daac1cb
MaciejMatczak2001 <maciej.matczak2001@gmail.com>
2023-05-12 10:24:56 +0200

    Adding POST method with validation

--- app.py
+++ app.py
@@ -3,15 +3,27 @@ import urllib
 
 import requests
 
-url = "https://mdn.github.io/learning-area/javascript/oojs/json/superheroes.json"
+# url = "https://mdn.github.io/learning-area/javascript/oojs/json/superheroes.json"
+url = "https://httpbin.org/post"
 
 
-def checkHTTP(url_link):
-    r = requests.get(url_link)
-    if 400 <= r.status_code < 600:
+def checkHTTP(response_code):
+    if 400 <= response_code < 600:
         raise Exception("error")
+
+
+def HTTP_GET(url_link):
+    r = requests.get(url_link)
+    checkHTTP(r.status_code)
     return r.json()
 
 
+def HTTP_POST(url_link):
+    body = {"name": "natalia"}
+    request = requests.post(url_link, json=body)
+    checkHTTP(request.status_code)
+    return request.json()
+
+
 if __name__ == '__main__':
-    print(checkHTTP(url))
+    print(HTTP_POST("https://httpbin.org/post"))
